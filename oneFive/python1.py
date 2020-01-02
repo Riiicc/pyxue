39af14356351f0eaa40d1c32080c1903f807cf0b
riiicc <Riiicc@hotmail.com>
2020-01-02 17:26:55 +0800

    12312

--- oneFive/python1.py
+++ oneFive/python1.py
@@ -2,3 +2,22 @@
 # for index in range(101):
 #     sum +=index
 # print(sum)
+
+sum = 0
+for x in range(1,100):
+    if x % 2==0:
+        sum+=x
+print(sum)
+
+x = int(input('x = '))
+y = int(input('y = '))
+# 如果x大于y就交换x和y的值
+if x > y:
+    # 通过下面的操作将y的值赋给x, 将x的值赋给y
+    x, y = y, x
+# 从两个数中较的数开始做递减的循环
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公约数是%d' % (x, y, factor))
+        print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+        break
