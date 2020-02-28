25bf2013077262701f9f6945f73a58aacbd452d9
ric <ric@example.com>
2020-02-28 21:55:17 +0800

    suanfa

--- oneFive/python1.py
+++ oneFive/python1.py
@@ -3,21 +3,48 @@
 #     sum +=index
 # print(sum)
 
-sum = 0
-for x in range(1,100):
-    if x % 2==0:
-        sum+=x
-print(sum)
+# sum = 0
+# for x in range(1,100):
+#     if x % 2==0:
+#         sum+=x
+# print(sum)
 
-x = int(input('x = '))
-y = int(input('y = '))
-# 如果x大于y就交换x和y的值
-if x > y:
-    # 通过下面的操作将y的值赋给x, 将x的值赋给y
-    x, y = y, x
-# 从两个数中较的数开始做递减的循环
-for factor in range(x, 0, -1):
-    if x % factor == 0 and y % factor == 0:
-        print('%d和%d的最大公约数是%d' % (x, y, factor))
-        print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
-        break
+# x = int(input('x = '))
+# y = int(input('y = '))
+# # 如果x大于y就交换x和y的值
+# if x > y:
+#     # 通过下面的操作将y的值赋给x, 将x的值赋给y
+#     x, y = y, x
+# # 从两个数中较的数开始做递减的循环
+# for factor in range(x, 0, -1):
+#     if x % factor == 0 and y % factor == 0:
+#         print('%d和%d的最大公约数是%d' % (x, y, factor))
+#         print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+#         break
+
+
+# a = 100
+# b = 12.345
+# c = 1 + 5j
+# d = 'hello, world'
+# e = True
+# print(type(a)) # <class 'int'>
+# print(type(b)) # <class 'float'>
+# print(type(c)) # <class 'complex'>
+# print(type(d)) # <class 'str'>
+# print(type(e)) # <class 'bool'>
+
+# 在参数名前面的*表示args是一个可变参数
+def add(*args):
+    total = 0
+    for val in args:
+        total += val
+    return total
+
+
+# 在调用add函数时可以传入0个或多个参数
+print(add())
+print(add(1))
+print(add(1, 2))
+print(add(1, 2, 3))
+print(add(1, 3, 5, 7, 9))
